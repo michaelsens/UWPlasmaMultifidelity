2d9695a2760d130ecc77063a27bb0425a87ecd43
Michael <michaelrobert.sensale@gmail.com>
2024-07-03 11:36:44 -0400

    total

--- NEAT/examples/optimize_loss_fraction_qs.py
+++ NEAT/examples/optimize_loss_fraction_qs.py
@@ -97,7 +97,7 @@ class optimize_loss_fraction:
 
         for B2c in B2cArr:
             self.field.B2c = B2c
-            self.field.Calculate()
+            self.field.calculate()
             print(np.sum((self.prob.residuals())**2))
 
         
